38195b9a701a0c1519bfe4b71179e1c6c0e2505f
yuntech <bayoolayiwola0gmail.com>
2023-07-13 15:16:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include -stdio.h
+#include <stdio.h>
 /**                                                * main - A program that print a line with puts function
  * Return: 0 (success)
  */
 int main(void)
 {                                                 puts("\"Programming is like building a multilingual puzzle");
-return (o);                                       }
+return (0);                                       }
