ec44f857a1584759a17a82ad8fd7b3fd31fbad88
yuntech <bayoolayiwola0gmail.com>
2023-07-13 14:20:11 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,9 @@
 /**
  * main - A program that print a line with puts function
  * return: 0 (sucess)
-*/
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-return(o);
+        return(o);
+}
