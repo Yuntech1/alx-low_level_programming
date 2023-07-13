82efa19c2a1a0ce9d063c7728fe55ca2c1819515
yuntech <bayoolayiwola0gmail.com>
2023-07-13 14:01:31 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include -stdio.h
+/**
+ * main - A program that print a line with puts function
+ * return:0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(o);
