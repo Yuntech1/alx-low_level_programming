087ddc3cdea110854f8290e71dd3b3fcc5a49140
yuntech <bayoolayiwola0gmail.com>
2023-07-18 07:44:27 -0700

    purchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,17 +3,17 @@
  * Main - entry point
  * Description - print a new line
  * Return: alway 0 (success)
-  */
+ */
 int main(void)
 {
-	int _purchar('_');
-	int _purchar('p');
-	int _purchar('u');
-	int _purchar('r');
-	int _purchar('c');
-	int _purchar('h');
-	int _purchar('a');
-	int _purchar('r');
-	int _purchar('\n');
+	 _purchar('_');
+	 _purchar('p');
+	 _purchar('u');
+	 _purchar('r');
+	 _purchar('c');
+	 _purchar('h');
+	 _purchar('a');
+	 _purchar('r');
+	 _purchar('\n');
 return (0);
 }
