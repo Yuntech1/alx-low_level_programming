3bd032f314fdb83b4dee270ae63ab9fae753b6be
yuntech <bayoolayiwola0gmail.com>
2023-08-24 13:37:30 -0700

    firstline

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+* print - prints before the main function
+* Return: void
+*/
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
