f2943a84507b67362495281b904e8144cffc0da2
yuntech <bayoolayiwola0gmail.com>
2023-08-29 11:22:35 -0700

    listint

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* listint_len - prints all the number of elements of a list_t.
+* @h: pointer to list.
+*
+* Return: the number of nodes.
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	int count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
